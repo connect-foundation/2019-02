44fdd6198a8bac308df7df7f58d20e5750a842f7
aereeeee <dofl5576@gmail.com>
2019-11-11 19:00:05 +0900

    [ ENV ] server-api 환경세팅: DB 연결 (#9)
    Why
    - 서비스에 필요한 데이터를 저장 할 DB에 연결이 필요함
    
    What
    - mongoose를 이용하여 원격 서버에 설치한 mongoDB를 연결함

--- server-api/src/app.js
+++ server-api/src/app.js
@@ -1,25 +1,26 @@
-const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
 class App {
-  constructor() {
+  constructor(db) {
     this.app = express();
     this.server = new ApolloServer({ typeDefs, resolvers });
+    this.db = db;
   }
+
   init() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cookieParser());
     this.server.applyMiddleware({ app: this.app });
   }
+
   start() {
-    this.app.listen({ port: 4000 }, () =>
-      console.log(`🚀 Server ready at http://localhost:4000${this.server.graphqlPath}`)
-    );
+    this.app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${this.server.graphqlPath}`));
   }
 }
 
-module.exports = new App();
+module.exports = App;
--- server-api/src/index.js
+++ server-api/src/index.js
@@ -2,6 +2,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const app = require('./app');
+const App = require('./app');
+const db = require('./models');
+
+const app = new App(db);
+
 app.init();
 app.start();
