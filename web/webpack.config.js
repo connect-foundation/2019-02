8222a43711b50398fab1af9aad0b36c283c07a8f
aereeeee <dofl5576@gmail.com>
2019-11-12 14:18:41 +0900

    [ ENV ] 클라이언트 개발 환경 세팅 (#9)
    Why
    - 리액트 환경에서 개발한 파일을 컴파일 해야 한다.
    - 원활한 협업을 위해 폴더 structure를 정해야 한다.
    
    What
    - webpack설정
    - 프로젝트 배포를 위한 public 폴더를 분리

---
+++ web/webpack.config.js
@@ -0,0 +1,40 @@
+const Path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: Path.join(__dirname, './build'),
+    filename: 'index.bundle.js',
+  },
+  resolve: {
+    alias: {
+      '@': Path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 8000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+};
