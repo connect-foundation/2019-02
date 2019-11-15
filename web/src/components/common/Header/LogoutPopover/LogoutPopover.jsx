f2c14bec161655c5b474cef20599ebafd694389c
happydhKim <happytime870616@gmail.com>
2019-11-15 21:10:40 +0900

    [ FEATURE ] 로그아웃 구현 (#10)
    Why
    - 회원이 로그아웃을 할 때, 헤더에 있는 토큰을 제거해 줘야 한다.
    - 회원이 로그아웃을 할 때, 로그인 상태를 변경해 줘야 한다.
    
    What
    - Mutation을 활용하여, 로그인 토큰을 제거 해준다.
    - Cache에서 로그인 정보를 제거해준다.

--- web/src/components/common/Header/LogoutPopover/LogoutPopover.jsx
+++ web/src/components/common/Header/LogoutPopover/LogoutPopover.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { useMutation } from '@apollo/react-hooks';
 import { MenuItem } from '@material-ui/core';
+import gql from 'graphql-tag';
+
+const LOGOUT = gql`
+  mutation LogOut($token: String!) {
+    logOut(token: $token) @client
+  }
+`;
 
 const LogoutPopover = (props) => {
   const { handleClose } = props;
-
+  const [logOut] = useMutation(LOGOUT);
+  const handleLogOut = () => {
+    handleClose();
+    logOut();
+  };
   return (
     <S.PopoverWrapper>
       <MenuItem onClick={handleClose}>
-      프로필
+        프로필
+      </MenuItem>
+      <MenuItem onClick={handleLogOut}>
+        로그아웃
       </MenuItem>
       <MenuItem onClick={handleClose}>
-      로그아웃
-      </MenuItem>
-      <MenuItem onClick={handleClose}>
-      도움말
+        도움말
       </MenuItem>
     </S.PopoverWrapper>
   );
