faf7cbeeb12f37606bc639d90f25c0217431fd42
aereeeee <dofl5576@gmail.com>
2019-11-20 18:17:34 +0900

    [ FEATURE ] 채널만들기 버튼 UI 구현 (#25)
    Why
    - 채널 생성을 위한 버튼 UI가 필요하다.
    
    What
    - ChannelButton 컴포넌트를 구현하였다.
    - theme에 ChannelButton 컴포넌트를 위한 스타일을 정의하였다.

---
+++ web/src/components/main/index.js
@@ -0,0 +1,1 @@
+export { default as ChannelButton } from './ChannelButton';
--- web/src/styles/theme.js
+++ web/src/styles/theme.js
@@ -45,6 +45,7 @@ export default createMuiTheme({
     },
     shadow: {
       button: 'rgba(0, 0, 0, 0.24) 0px 2px 2px 0px, rgba(0, 0, 0, 0.24) 0px 0px 1px 0px',
+      channelButton: '0px 2px 19px rgba(230, 119, 0, 0.4)',
     },
   },
   typography: {
