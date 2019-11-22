9c932c3c42724b02dedea73a02a067626a267346
happydhKim <happytime870616@gmail.com>
2019-11-22 11:22:25 +0900

    [ REVIEW ] 김도현의 리뷰요청입니다.

---
+++ server-converter/src/@types/index.d.ts
@@ -0,0 +1,2 @@
+export * from './express.d';
+export * from './converter.d';
---
+++ server-converter/src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import * as jwt from 'jsonwebtoken';
+import { RequestHandler } from '../@types';
+
+const verifyToken = (token: string): boolean => {
+  if (!token) return false;
+
+  try {
+    const payload: any = jwt.verify(token, process.env.TOKEN_SECRET);
+    return !!(payload.id && payload.displayname);
+  } catch (error) {
+    return false;
+  }
+};
+
+const authMiddleware: RequestHandler = (req, res, next) => {
+  const tokenKey: string = 'x-auth-token';
+  const token: string = req.headers[tokenKey] as string;
+  const isVerified: boolean = verifyToken(token);
+
+  if (isVerified) {
+    next();
+  } else {
+    res.status(401).json({ status: 'unauthorized' });
+  }
+};
+
+export default authMiddleware;
