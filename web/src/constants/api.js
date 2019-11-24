643bd76390e671e86bfcf80c39b302b65de8555b
happydhKim <happytime870616@gmail.com>
2019-11-24 20:45:25 +0900

    [ FEATURE ] Web과 server-api 소켓 연동 구현 (#24)
    Why
    - 채팅을 하기 위해 소켓 연동이 필요하다.
    
    What
    - apollo를 이용해 소켓 연동 구현.

--- web/src/constants/api.js
+++ web/src/constants/api.js
@@ -1,8 +1,10 @@
 const API_HOST = 'http://localhost:4000';
+const WS_HOST = 'ws://localhost:4000';
 const CONVERTER_HOST = 'http://localhost:3000';
 
 export const CONVERT_API = `${CONVERTER_HOST}/images`;
 export const GRAPHQL_API = `${API_HOST}/graphql`;
+export const GRAPHQL_WS_API = `${WS_HOST}/graphql`;
 export const KAKAO_AUTH_API = `${API_HOST}/auth/kakao`;
 export const GOOGLE_AUTH_API = `${API_HOST}/auth/google`;
 export const NAVER_AUTH_API = `${API_HOST}/auth/naver`;
