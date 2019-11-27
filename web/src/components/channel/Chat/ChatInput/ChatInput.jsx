6816cf89f5de563704b82c8c4dab26cb65df9eeb
load0ne <jjazan201@gmail.com>
2019-11-27 10:54:35 +0900

    [ FEATURE ] 채팅 전송시 Input 클리어 및 엔터키로 전송 (#55)
    Why
    - UX 측면에서 엔터키를 이용해 전송이 가능해야한다.
    - 전송이 되었다면 채팅창이 클리어되어야한다.
    
    What
    - ChatInput 컴포넌트에서 채팅 전송시 message state 초기화
    - ChatInput 컴포넌트에서 Enter Key 에 대한 이벤트 추가
    - ChatLogs 에서 엔터키 적용 가능하도록 수정

--- web/src/components/channel/Chat/ChatInput/ChatInput.jsx
+++ web/src/components/channel/Chat/ChatInput/ChatInput.jsx
@@ -3,27 +3,40 @@ import PropTypes from 'prop-types';
 import S from './style';
 import { useAddChat } from '@/hooks';
 
+const KEYCODE_ENTER = 13;
+
 const ChatInput = (props) => {
-  const [state, setState] = useState('');
+  const [message, setMessage] = useState('');
   const { mutate } = useAddChat();
   const { channelId } = props;
-  const handleClick = () => {
-    mutate({ variables: { channelId, message: state } });
+  const sendMessage = () => {
+    if (message === '') return;
+
+    mutate({ variables: { channelId, message } });
+    setMessage('');
   };
-  const handleChange = (event) => {
-    setState(event.target.value);
+  const handleChangeInput = (event) => {
+    if (event);
+    setMessage(event.target.value);
+  };
+  const handleKeyDownInput = (event) => {
+    if (event.keyCode === KEYCODE_ENTER && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage();
+    }
   };
 
   return (
     <S.ChatInputWrapper>
       <S.MessageInput
-        placeholder="질문 또는 의견을 남겨주세요."
-        onChange={handleChange}
-        value={state}
+        placeholder="의견을 남겨주세요."
+        onChange={handleChangeInput}
+        onKeyDown={handleKeyDownInput}
+        value={message}
       />
       <S.SendButton
         type="button"
-        onClick={handleClick}
+        onClick={sendMessage}
       >
         전송
       </S.SendButton>
