ce4d84d8ae21adc28144f0ac1eee0f4f494048ca
always-awake <leemirim9654@gmail.com>
2019-11-27 14:52:31 +0900

    [ UTIL ] 자주 사용하는 main color, light-main color를 리턴해주는 유틸함수 구현
    Why
    - 컴포넌트 스타일 전반적으로 자주 사용되는 색정보를 바로 불러올 수 있도록 저리할 필요가 있다.
    
    What
    - theme 객체를 이용하여 메인 색을 조회하는 colorMain 유틸 함수 구현
    - theme 객체를 이용하여 light 메인 색을 조회하는 colorLightMain 유틸 함수 구현

--- web/src/styles/themeUtil.js
+++ web/src/styles/themeUtil.js
@@ -1,10 +1,13 @@
 const px = (val) => ({ theme }) => theme.typography.pxToRem(val);
 const colorGray = (val) => ({ theme }) => theme.palette.dropyGray[val];
 const colorYellow = (val) => ({ theme }) => theme.palette.dropyYellow[val];
-
+const colorMain = () => ({ theme }) => theme.palette.primary.main;
+const colorLightMain = () => ({ theme }) => theme.palette.primary.light;
 
 export {
   px,
   colorGray,
   colorYellow,
+  colorMain,
+  colorLightMain,
 };
