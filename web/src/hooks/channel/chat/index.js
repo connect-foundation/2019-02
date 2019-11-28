3e675e53e8fb4b21353589e32af7b1321c5cb05f
load0ne <jjazan201@gmail.com>
2019-11-28 21:49:59 +0900

    [ REFACTOR ] 훅 디렉토리 추상화 구조 정리
    Why
    - hook 파일이 많아짐에 따라서, 디렉토리를 추상화해서 파일 분리할 필요성을 느꼈다.
    
    What
    - hook 을 컴포넌트 구조와 동일하게 맞췄다.

---
+++ web/src/hooks/channel/chat/index.js
@@ -0,0 +1,7 @@
+export { default as useAddChat } from './useAddChat';
+export { default as useChangeChatSort } from './useChangeChatSort';
+export { default as useChatChanged } from './useChatChanged';
+export { default as useGetChatsCached } from './useGetChatsCached';
+export { default as useInitChatCached } from './useInitChatCached';
+export { default as useInitChatLogs } from './useInitChatLogs';
+export { default as useLikeChat } from './useLikeChat';
---
+++ web/src/hooks/channel/slide/index.js
@@ -0,0 +1,3 @@
+export { default as useSetCurrentSlide } from './useSetCurrentSlide';
+export { default as useSlideChanged } from './useSlideChanged';
+export { default as useSyncSlide } from './useSyncSlide';
