6ddb03bc8b717f4dbb6d008139651089d560dd64
happydhKim <happytime870616@gmail.com>
2019-12-03 01:01:29 +0900

    [ FEATURE ] 채널 내 다운로드 버튼 생성 (#82)
    Why
    - 채널 내 자료를 다운로드 할 수 있는 버튼이 필요하다.
    
    What
    - 다운로드 버튼 생성.

--- web/src/components/channel/Slide/SlideInfo/SlideDownloadButton/SlideDownloadButton.jsx
+++ web/src/components/channel/Slide/SlideInfo/SlideDownloadButton/SlideDownloadButton.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { SmallButton } from '@/components/common';
+import { useGetChannel } from '@/hooks';
 
-const SlideDownloadButton = () => (
-  <></>
-);
+
+const SlideDownloadButton = () => {
+  const channelInfo = useGetChannel();
+  return (
+    <>
+      <SmallButton>
+        <span aria-label="slide-code-share-button" role="img">💾</span>
+        <span>다운로드</span>
+      </SmallButton>
+    </>
+  );
+};
 
 export default SlideDownloadButton;
