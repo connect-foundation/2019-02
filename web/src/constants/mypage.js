45b726664e22d76baeabd43076d866fe58b1c2ef
happydhKim <happytime870616@gmail.com>
2019-12-09 20:40:12 +0900

    [ REFACTOR ] 마이페이지는 로그인을 해야만 입장이 가능하다. (#122)
    Why
    - 익명의 사용자가 마이페이지에 들어갔을 때, 어색한 현상들이 많이 일어난다.
    
    What
    - 로그인을 하지 않은 사용자는 마이페이지에 들어오면 접근이 제한되도록 구현.
    - 접근이 제한 된 것은 ErrorModal을 이용함.
    - 에러메시지를 위해 constants 에 mypage 생성.

---
+++ web/src/constants/mypage.js
@@ -0,0 +1,3 @@
+const LOGIN_STATUS_ERROR_MESSAGE = '로그인을 해주셔야 이용이 가능합니다.';
+
+export default LOGIN_STATUS_ERROR_MESSAGE;
--- web/src/pages/MyPage/MyPage.jsx
+++ web/src/pages/MyPage/MyPage.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
+import { useGetUserStatus } from '@/hooks';
+import ErrorModal from '@/components/common/ErrorModal';
 import { UserInfo, UserHistory } from '@/components/history';
+import LOGIN_STATUS_ERROR_MESSAGE from '@/constants/mypage';
 import S from './style';
 
 const MyPage = () => {
   const [historyState, setHistoryState] = useState('speaker');
+  const { displayName } = useGetUserStatus();
 
   return (
     <>
-      <S.MyPageWrapper>
-        <UserInfo setHistoryState={setHistoryState} />
-        <UserHistory historyState={historyState} />
-      </S.MyPageWrapper>
+      {displayName !== null
+        ? (
+          <S.MyPageWrapper>
+            <UserInfo setHistoryState={setHistoryState} />
+            <UserHistory historyState={historyState} />
+          </S.MyPageWrapper>
+        )
+        : <ErrorModal message={LOGIN_STATUS_ERROR_MESSAGE} />}
     </>
   );
 };
