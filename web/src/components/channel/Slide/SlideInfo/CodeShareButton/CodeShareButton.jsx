ba3d4b2720c611371f93a45efac1c1d2237f2da4
happydhKim <happytime870616@gmail.com>
2019-12-09 17:36:01 +0900

    [ FEATURE ] 채널코드가 복사 되면, 알림 표시가 필요하다. (#123)
    Why
    - 채널코드가 복사 되면, 알림 표시가 나타나야 한다.
    
    What
    - Mertirial UI의 tooltip을 세팅한다.
    - hover가 아닌 버튼을 눌렀을 경우 tooltip을 보이게 한다.
    - 3초후에 자동으로 사라지게 한다.

--- web/src/components/channel/Slide/SlideInfo/CodeShareButton/CodeShareButton.jsx
+++ web/src/components/channel/Slide/SlideInfo/CodeShareButton/CodeShareButton.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import { SmallButton } from '@/components/common';
+import React, { useState } from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { useChannelSelector } from '@/hooks';
 import { copyToClipboard } from '@/utils/dom';
+import S from './style';
 
 const CodeShareButton = () => {
+  const [open, setOpen] = useState(false);
   const channelCode = useChannelSelector((state) => state.channelCode);
-  const handleCodeShareButtonOnClick = () => {
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+  const handleTooltipOpen = () => {
+    setOpen(true);
     copyToClipboard(channelCode);
+    setTimeout(() => {
+      setOpen(false);
+    }, 3000);
   };
 
   return (
-    <SmallButton onClick={handleCodeShareButtonOnClick}>
-      <span aria-label="slide-code-share-button" role="img">📎</span>
-      <span>채널 공유</span>
-    </SmallButton>
+    <ClickAwayListener onClickAway={handleTooltipClose}>
+      <Tooltip
+        PopperProps={{
+          disablePortal: true,
+        }}
+        onClose={handleTooltipClose}
+        open={open}
+        disableFocusListener
+        disableHoverListener
+        disableTouchListener
+        title="채널코드 복사완료"
+      >
+        <S.SmallButton
+          onClick={handleTooltipOpen}
+          variant="contained"
+        >
+          <span aria-label="slide-code-share-button" role="img">📎</span>
+          <span>채널 공유</span>
+        </S.SmallButton>
+      </Tooltip>
+    </ClickAwayListener>
   );
 };
 
