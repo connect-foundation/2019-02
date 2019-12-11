917b434e57b85aeb6206ad6d33e90c851ba56c8c
aereeeee <dofl5576@gmail.com>
2019-12-11 20:30:29 +0900

    [ FIX ] 해쉬태그 페이지 0 오류 수정 (#130)
    Why
    - 페이지 0 은 존재하지 않는다.
    
    What
    - 페이지 0을 예외처리한다

--- web/src/utils/tagParser.js
+++ web/src/utils/tagParser.js
@@ -1,6 +1,6 @@
 const tagParser = (token, limit) => {
-  const nextPage = token.split('#')[1];
-  const isExist = nextPage <= limit;
+  const nextPage = +token.split('#')[1];
+  const isExist = !!nextPage && nextPage <= limit;
 
   return { nextPage, isExist };
 };
