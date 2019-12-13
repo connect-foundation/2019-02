1f9aab633ccd180d82fefc36325e909877305b0f
aereeeee <dofl5576@gmail.com>
2019-12-13 10:43:58 +0900

    [ FEATURE ] 채널 옵션 설정을 위한 커스텀 훅 구현 (#131)
    Why
    - 클라이언트에서 mutation, subscription을 통해 데이터를 페치해와야 한다.
    - 변경된 데이터 타입에 따라 클라이언트 쿼리 필드를 수정해야한다.
    
    What
    - 여러 컴포넌트에서 사용할 수 있도록 채널 옵션 설정 관련 graphQL로직을 커스텀 훅으로 작성
    - channel데이터 타입에 channelOptions필드 추가

--- web/src/hooks/channel/settings/index.js
+++ web/src/hooks/channel/settings/index.js
@@ -1,2 +1,4 @@
-/* eslint-disable import/prefer-default-export */
-export { default as useUpdatePresentationSettings } from './useUpdatePresentationSettings';
+export { default as useUpdateChannelOptions } from './useUpdateChannelOptions';
+export { default as useAnonymousChanged } from './useAnonymousChanged';
+export { default as useChannelNameChanged } from './useChannelNameChanged';
+export { default as useEmojiChanged } from './useEmojiChanged';
