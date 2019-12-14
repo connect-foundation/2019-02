84ffbae54e3b01319fbb2e4c90b50296f0df3fac
always-awake <leemirim9654@gmail.com>
2019-12-14 22:18:59 +0900

    [ UTIL ] DropyCanvas 모듈 구현 (#144)
    Why
    - 캔버스를 사용하는 로직이 굉장히 중복되고, 일관성있기 때문에 이를 모듈화 하였다.
    
    What
    - ES6 class 문법을 이용해 구현하였다.
    - 캔버스의 높이와 길이를 DropyCanvas 객체를 생성할 수 있다.
    - render 함수는 mouseenter, mouseleave, mousemove 등의 이벤트처리하는 핸들러 함수를 갖고 있는 canvas 엘리먼트를 리턴한다.
    - DropyCanvas의 getTempCanvasHistory 함수를 이용해 해당 객체가 저장하고 있는 좌표 값(그림의 위치) 리스트를 리턴받을 수 있다.
    - 이후에 좌표 값이 있는 리스트와 canvas context를 매개변수로 갖는 reDrawContent 함수를 호출하면, 새로운 캔버스에 기존 캔버스에서 그렸던 그림을 그대로 그릴 수 있다.

---
+++ web/src/utils/DropyCanvas/DropyCanvas.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import {
+  ratioToRealPosition,
+  addHandlerToEvent,
+  removeHandlerFromEvent,
+} from './utils';
+
+class DropyCanvas {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  init() {
+    this.prevPosition = { x: 0, y: 0 };
+    this.tempCanvasHistory = [];
+    this.toolType = null;
+    this.lineWidth = null;
+    this.lineCap = null;
+    this.strokeStyle = null;
+    this.eventAndHandlerList = [
+      ['mousemove', this.handleMouseMove.bind(this)],
+      ['mouseup', this.handleMouseUp.bind(this)],
+      ['mousedown', this.handleMouseDown.bind(this)],
+      ['mouseleave', this.handleMouseLeave.bind(this)],
+      ['mouseenter', this.handleMouseEnter.bind(this)],
+    ];
+  }
+
+  handleMouseDown(event) {
+    this.prevPosition.x = event.offsetX;
+    this.prevPosition.y = event.offsetY;
+  }
+
+  handleMouseMove(event) {
+    if (event.buttons !== 1) return;
+
+    this.context.beginPath();
+
+    this.context.lineWidth = this.lineWidth;
+    this.context.lineCap = this.lineCap;
+    this.context.strokeStyle = this.strokeStyle;
+
+    this.context.moveTo(this.prevPosition.x, this.prevPosition.y);
+    this.context.lineTo(event.offsetX, event.offsetY);
+    this.context.stroke();
+
+    this.saveMousePosition(event.offsetX, event.offsetY);
+    this.addCanvasHistory(event.offsetX, event.offsetY);
+  }
+
+  handleMouseEnter(event) {
+    this.prevPosition.x = event.offsetX;
+    this.prevPosition.y = event.offsetY;
+  }
+
+  handleMouseUp() {
+    this.tempCanvasHistory.push([]);
+  }
+
+  handleMouseLeave() {
+    this.tempCanvasHistory.push([]);
+  }
+
+  addEventListener(canvasElement) {
+    addHandlerToEvent(canvasElement, this.eventAndHandlerList);
+  }
+
+  removeEventListener(canvasElement) {
+    removeHandlerFromEvent(canvasElement, this.eventAndHandlerList);
+  }
+
+  addCanvasHistory(mousePositionX, mousePositionY) {
+    const ratioX = this.width / mousePositionX;
+    const ratioY = this.height / mousePositionY;
+
+    this.tempCanvasHistory.push([ratioX, ratioY]);
+  }
+
+  saveMousePosition(x, y) {
+    this.prevPosition.x = x;
+    this.prevPosition.y = y;
+  }
+
+  setContext(context) {
+    this.context = context;
+  }
+
+  setTool(toolOption) {
+    const {
+      toolType,
+      toolStyleOption: {
+        lineWidth,
+        lineCap,
+        lineColor,
+      },
+    } = toolOption;
+
+    this.toolType = toolType;
+    this.lineWidth = lineWidth;
+    this.lineCap = lineCap;
+    this.strokeStyle = lineColor;
+  }
+
+  getTempCanvasHistory() {
+    return this.tempCanvasHistory;
+  }
+
+  reDrawContent(canvasHistory, context) {
+    const newPosition = [];
+    canvasHistory.forEach((history, index) => {
+      const ratioX = history[0];
+      const ratioY = history[1];
+      const { currPositionX, currPositionY } = ratioToRealPosition(
+        ratioX,
+        ratioY,
+        this.width,
+        this.height,
+      );
+
+      newPosition.push([currPositionX, currPositionY]);
+
+      if (index === 0) return;
+
+      const prevNewPosition = newPosition[index - 1];
+      const prevNewPositionX = prevNewPosition[0];
+      const prevNewPositionY = prevNewPosition[1];
+
+      context.lineWidth = this.lineWidth;
+      context.lineCap = this.lineCap;
+      context.strokeStyle = this.strokeStyle;
+
+      context.beginPath();
+      context.moveTo(prevNewPositionX, prevNewPositionY);
+      context.lineTo(currPositionX, currPositionY);
+      context.stroke();
+    });
+  }
+
+  render(canvasRef) {
+    return (
+      <canvas
+        ref={canvasRef}
+        width={this.width}
+        height={this.height}
+      />
+    );
+  }
+}
+
+export default DropyCanvas;
